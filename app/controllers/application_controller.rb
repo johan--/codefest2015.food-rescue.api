e898da828dd1c28e2836e2ca6b61dd6283f2d28d
Kyle Szives <kjszives@gmail.com>
2015-02-21 13:45:12 -0500

    removed this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,27 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
-  skip_before_filter :verify_authenticity_token
+  # before_filter :cors_preflight_check
+  # after_filter :cors_set_access_control_headers
 
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+  # def cors_set_access_control_headers
+  #   headers['Access-Control-Allow-Origin'] = '*'
+  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+  #   headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  #   headers['Access-Control-Max-Age'] = "1728000"
+  # end
 
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
+  # def cors_preflight_check
+  #   debugger
+  #   if request.method == 'OPTIONS'
+  #     headers['Access-Control-Allow-Origin'] = '*'
+  #     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+  #     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+  #     headers['Access-Control-Max-Age'] = '1728000'
 
-  def cors_preflight_check
-    debugger
-    if request.method == 'OPTIONS'
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-      headers['Access-Control-Max-Age'] = '1728000'
-
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
+  #     render :text => '', :content_type => 'text/plain'
+  #   end
+  # end
 
   def verify_authentication_token!
     authenticate_or_request_with_http_basic do |user_name, token|
