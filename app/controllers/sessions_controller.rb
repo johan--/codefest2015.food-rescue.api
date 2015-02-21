7e08859aebfad6a586b94a7560882ab995b31f15
Kyle Szives <kjszives@gmail.com>
2015-02-21 11:43:30 -0500

    return type now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,12 @@ class SessionsController < Devise::SessionsController
   def create
     warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
     render :status => 200,
-           :json => { :success => true,
-                      :info => "Logged in",
-                      :data => { :auth_token => current_user.authentication_token } }
+           :json => { success: true,
+                      info: "Logged in",
+                      data: {
+                        auth_token: current_user.authentication_token,
+                        type: current_user.type
+                      } }
   end
 
   def destroy
