d5ac6ef8743435468412392691a015de86ef27b7
Kyle Szives <kjszives@gmail.com>
2015-02-21 13:24:13 -0500

    added CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,28 @@
 class ApplicationController < ActionController::Base
-  before_action :set_headers_for_ionic
+  skip_before_action :verify_authenticity_token
 
+  skip_before_filter :verify_authenticity_token
 
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
 
   def verify_authentication_token!
     authenticate_or_request_with_http_basic do |user_name, token|
@@ -14,9 +35,5 @@ class ApplicationController < ActionController::Base
 
       return true
     end
-  end
-
-  def set_headers_for_ionic
-    response.headers("Access-Control-Allow-Origin", "*");
   end
 end
