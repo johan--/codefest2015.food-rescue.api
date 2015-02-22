9c35e6ade0238931ae2f6c94d8d2e4883e0a2106
Kyle Szives <kjszives@gmail.com>
2015-02-21 19:19:51 -0500

    added d

--- app/controllers/api/v1/donations_controller.rb
+++ app/controllers/api/v1/donations_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DonationsController < ApplicationController
   before_action :verify_authentication_token!
   before_action :verify_donor
 
-  before_action :set_donation, only: [:show, :update]
+  before_action :set_donation, only: [:show, :update, :delete ]
 
   def index
     render json: current_user.donations
